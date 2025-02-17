be44b1212c2b46a72d7b6e504b70460a9d7bf3e0
Gustavo H. K. Oliveira <123880096+gustavo-h-k-oliveira@users.noreply.github.com>
2025-02-17 17:57:15 +0000

    Adiciona suporte a métodos GET e POST no `IndexController`.

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -1,12 +1,20 @@
 package application.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-    @RequestMapping("/")
+    // Método GET
+    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    // Método POST
+    @PostMapping("/")
+    public String postHome() {
+        return "post home";
     }
 }
