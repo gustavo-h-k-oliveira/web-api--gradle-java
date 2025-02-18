9b3cfd7e6d691f055b036b86af8804c19a539af9
Gustavo H. K. Oliveira <123880096+gustavo-h-k-oliveira@users.noreply.github.com>
2025-02-18 16:41:26 +0000

    Simplifica mapeamento de rotas no `IndexController` removendo caminhos explícitos para métodos GET e POST.

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -3,17 +3,19 @@ package application.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/")
 public class IndexController {
     // Método GET
-    @GetMapping("/")
+    @GetMapping
     public String home() {
         return "home";
     }
 
     // Método POST
-    @PostMapping("/")
+    @PostMapping
     public String postHome() {
         return "post home";
     }
