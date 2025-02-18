1cded80207a5d457dd65f05c1dc9c4e897c1536b
Gustavo H. K. Oliveira <123880096+gustavo-h-k-oliveira@users.noreply.github.com>
2025-02-18 17:31:41 +0000

    Atualização do `IndexController` para retornar e manipular uma lista de frutas com métodos GET e POST.

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -1,22 +1,32 @@
 package application.controller;
 
+// Importações necessárias
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/")
 public class IndexController {
-    // Método GET
+    // Lista de frutas inicializada como uma lista vazia
+    // private List<String> frutas = Arrays.asList("Maça", "Banana", "Morango");
+    private List<String> frutas = new ArrayList<String>();
+    
+    // Método GET para retornar a lista de frutas
     @GetMapping
-    public String home() {
-        return "home";
+    public List<String> home() {
+        return frutas;
     }
 
-    // Método POST
+    // Método POST para adicionar uma fruta à lista e retornar a lista atualizada
     @PostMapping
-    public String postHome() {
-        return "post home";
+    public List<String> postHome(@RequestBody String fruta) {
+        frutas.add(fruta);
+        return frutas;
     }
 }
